aab132296c0a1510c69ca0d90da4b9d6b1c19e8d
MisterMario <aim.power2@gmail.com>
2019-04-11 15:19:41 +0300

    Добавлен выпадающий список результатов для поиска в index.html
    Добавлен выпадающий список результатов для поиска. Подключена библеотека jQuery. Создана папка для клиентских скриптов.

---
+++ js/drop-search-results.js
@@ -0,0 +1,11 @@
+function animateDropResults(self) {
+  var value = $(self).val(),
+      drop_list = $('#search-results');
+
+  if (value.length > 0 && drop_list.css('display') == 'none')
+    drop_list.slideDown(500);
+  else if (value.length == 0) {
+    drop_list.css('margin-top', '0');
+    drop_list.slideUp(500);
+  }
+}
